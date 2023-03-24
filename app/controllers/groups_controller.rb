0c5dfc3c048e51507bfca29f2a373890ee93b91d
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2023-03-25 02:56:05 +0300

    add cntroller and view

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,43 @@
+class GroupsController < ApplicationController
+  before_action :set_group, only: %i[show edit update destroy]
+
+  # GET /groups or /groups.json
+  def index
+    @groups = Group.all
+  end
+
+  # GET /groups/1 or /groups/1.json
+  def show; end
+
+  # GET /groups/new
+  def new
+    @group = Group.new
+  end
+
+  # POST /groups or /groups.json
+  def create
+    @group = Group.new(group_params)
+
+    respond_to do |format|
+      if @group.save
+        format.html { redirect_to groups_url, notice: 'Group was successfully created.' }
+        format.json { render :show, status: :created, location: @group }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @group.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def group_params
+    params.require(:group).permit(:name, :icon, :user_id)
+  end
+end
